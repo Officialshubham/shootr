986a89d45f8b7e8ad39bc75b4a6445fc4bc50d20
Shubham Sharma <shubhamsharma.100799@gmail.com>
2025-07-22 19:47:44 +1000

    Remove space

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, ease: 'easeOut' }}
       >
-        Shootr Game
+      Shootr Game
       </motion.h2>
 
       <motion.div
